1a9970db92c66824678b0702722a5d99f1282ce2
Josué Chaves <d4n11083@estudiantec.cr>
2023-09-07 09:44:15 -0600

    Create PrettyPrint.java

---
+++ src/PrettyPrint.java
@@ -0,0 +1,115 @@
+import generated.AlphaParser;
+import generated.AlphaParserBaseVisitor;
+
+public class PrettyPrint extends AlphaParserBaseVisitor<Object> {
+
+    @Override
+    public Object visitProgramAST(AlphaParser.ProgramASTContext ctx) {
+        return super.visitProgramAST(ctx);
+    }
+
+    @Override
+    public Object visitCommandAST(AlphaParser.CommandASTContext ctx) {
+        return super.visitCommandAST(ctx);
+    }
+
+    @Override
+    public Object visitAssignSCAST(AlphaParser.AssignSCASTContext ctx) {
+        return super.visitAssignSCAST(ctx);
+    }
+
+    @Override
+    public Object visitCallSCAST(AlphaParser.CallSCASTContext ctx) {
+        return super.visitCallSCAST(ctx);
+    }
+
+    @Override
+    public Object visitIfSCAST(AlphaParser.IfSCASTContext ctx) {
+        return super.visitIfSCAST(ctx);
+    }
+
+    @Override
+    public Object visitWhileSCAST(AlphaParser.WhileSCASTContext ctx) {
+        return super.visitWhileSCAST(ctx);
+    }
+
+    @Override
+    public Object visitLetSCAST(AlphaParser.LetSCASTContext ctx) {
+        return super.visitLetSCAST(ctx);
+    }
+
+    @Override
+    public Object visitBlockSCAST(AlphaParser.BlockSCASTContext ctx) {
+        return super.visitBlockSCAST(ctx);
+    }
+
+    @Override
+    public Object visitDeclarationAST(AlphaParser.DeclarationASTContext ctx) {
+        return super.visitDeclarationAST(ctx);
+    }
+
+    @Override
+    public Object visitConstSDAST(AlphaParser.ConstSDASTContext ctx) {
+        return super.visitConstSDAST(ctx);
+    }
+
+    @Override
+    public Object visitVarSDAST(AlphaParser.VarSDASTContext ctx) {
+        return super.visitVarSDAST(ctx);
+    }
+
+    @Override
+    public Object visitDefSDAST(AlphaParser.DefSDASTContext ctx) {
+        return super.visitDefSDAST(ctx);
+    }
+
+    @Override
+    public Object visitIntTDAST(AlphaParser.IntTDASTContext ctx) {
+        return super.visitIntTDAST(ctx);
+    }
+
+    @Override
+    public Object visitCharTDAST(AlphaParser.CharTDASTContext ctx) {
+        return super.visitCharTDAST(ctx);
+    }
+
+    @Override
+    public Object visitStringTDAST(AlphaParser.StringTDASTContext ctx) {
+        return super.visitStringTDAST(ctx);
+    }
+
+    @Override
+    public Object visitExpressionAST(AlphaParser.ExpressionASTContext ctx) {
+        return super.visitExpressionAST(ctx);
+    }
+
+    @Override
+    public Object visitNumPEAST(AlphaParser.NumPEASTContext ctx) {
+        return super.visitNumPEAST(ctx);
+    }
+
+    @Override
+    public Object visitIdPEAST(AlphaParser.IdPEASTContext ctx) {
+        return super.visitIdPEAST(ctx);
+    }
+
+    @Override
+    public Object visitExpPEAST(AlphaParser.ExpPEASTContext ctx) {
+        return super.visitExpPEAST(ctx);
+    }
+
+    @Override
+    public Object visitCharPEAST(AlphaParser.CharPEASTContext ctx) {
+        return super.visitCharPEAST(ctx);
+    }
+
+    @Override
+    public Object visitStrPEAST(AlphaParser.StrPEASTContext ctx) {
+        return super.visitStrPEAST(ctx);
+    }
+
+    @Override
+    public Object visitOperator(AlphaParser.OperatorContext ctx) {
+        return super.visitOperator(ctx);
+    }
+}
